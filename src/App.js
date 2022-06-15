f8b904ff8ae3abed6079c57f161fe32f00725fb4
Chowdhury Farhan Jamil <chy.farhan4@gmail.com>
2022-06-15 15:49:18 +0600

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {BrowserRouter as router, Switch, Route}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Main page
+      </header>
+    </div>
+  );
+}
+
+export default App;
