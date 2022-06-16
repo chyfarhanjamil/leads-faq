074bf53cffc36b42743053cbf7487971b2c28187
Chowdhury Farhan Jamil <chy.farhan4@gmail.com>
2022-06-16 13:13:38 +0600

    on progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import {BrowserRouter as router, Switch, Route, Link} from 'react-router-dom';
+import React, { useState, useRef } from "react";
+import "./App.scss";
+import { BrowserRouter as router, Switch, Route, Link } from "react-router-dom";
+import Login from "./components/authentication/Login";
 
 function App() {
+  const [isLogginActive, setLogginActive] = useState(true);
+  const containerRef = useRef(null);
+  
   return (
     <div className="App">
-      app
+      <div className="login">
+        <div className="container">{isLogginActive && <Login />}</div>
+      </div>
     </div>
   );
 }
--- src/components/Register/Register.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./Register.scss";
-
-export default function Register() {
-  return <div>Register</div>;
-}
